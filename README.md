56fb9b8b7e12592f7b564c802ebda293e3191a54
Bobby Iliev <bobby@bobbyiliev.com>
2020-06-18 16:05:28 +0000

    Cloudflare DDoS Protection script

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# cloudflare-ddos-protection
+# Cloudflare DDoS Protection Script
+
+Cloudflare offers free DDoS protection and they have a cool API that you could use to enable and disable their DDoS protection easily.
+
+You can use this CLI script to enable and disable the CloudFlare DDOS protection for your website automatically based on the CPU load of your server.
+
+## Prerequisites
+
+* A Cloudflare account
+* Cloudflare API key
+* Cloudflare Zone ID
+* Make sure curl is installed on your server: `curl --version`
+
+If curl is not installed you need to run the following:
+
+For RedHat/CentOs:
+
+```
+yum install curl
+```
+For Debian/Ubuntu
+
+```
+apt-get install curl
+```
+
+## Setup
+
+All you need to do is to download the script and save it on your server.
+
+Make the script executable:
+
+```
+chmod +x ~/protection.sh
+```
+
+Setup 2 Cron jobs to run every 30 seconds. To edit your crontab run:
+
+```
+crontab -e
+```
+
+And add the following content:
+
+```
+* * * * * /path-to-the-script/cloudflare/protection.sh
+* * * * * ( sleep 30 ; /path-to-the-script/cloudflare/protection.sh )
+```
---
+++ protection.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+###
+# Automating your CloudFlare DDoS Protection by https://bobbyiliev.com
+###
+
+##
+# Cloud Flare API Config
+##
+
+CF_CONE_ID=YOUR_CF_ZONE_ID
+CF_EMAIL_ADDRESS=YOUR_CF_EMAIL_ADDRESS
+CF_API_KEY=YOUR_CF_API_KEY
+
+##
+# Set to 1 in order to enable email notifications
+##
+notifications=1
+
+##
+# Prepare CloudFlare directory
+if ! [ -d ~/.cloudflare ] ; then
+    mkdir ~/.cloudflare
+fi
+
+##
+##
+# Check current status:
+##
+
+current_status=$(mktemp /tmp/temp-status.XXXXXX)
+status=$(mktemp /tmp/temp-status.XXXXXX)
+
+function status() {
+    curl -X GET "https://api.cloudflare.com/client/v4/zones/${CF_CONE_ID}/settings/security_level" \
+        -H "X-Auth-Email: ${CF_EMAIL_ADDRESS}" \
+        -H "X-Auth-Key: ${CF_API_KEY}" \
+        -H "Content-Type: application/json" 2>/dev/null > ${current_status}
+
+    cat ${current_status} | awk -F":" '{ print $4 }' | awk -F',' '{ print $1 }' | tr -d '"' > ${status}
+    currentStatus=$(cat ${status})
+}
+
+##
+# Monitring your CPU load:
+##
+
+load=$(uptime | awk -F'average:' '{ print $2 }' | awk '{print $1}' | sed 's/,/ /')
+
+ddos=${load%.*}
+
+##
+# Monitor the status and enable the DDoS protection if required:
+##
+
+function allowed_cpu_load(){
+    normalCPUload=$(grep -c ^processor /proc/cpuinfo);
+    maxCPUload=$(( $normalCPUload*2 ));
+}
+
+function normal_cpu_load(){
+    normalCPUload=6;
+}
+
+function disable(){
+    curl -X PATCH "https://api.cloudflare.com/client/v4/zones/${CF_CONE_ID}/settings/security_level" \
+     -H "X-Auth-Email: ${CF_EMAIL_ADDRESS}" \
+     -H "X-Auth-Key: ${CF_API_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"value":"medium"}'
+}
+
+function under_attack(){
+    curl -X PATCH "https://api.cloudflare.com/client/v4/zones/${CF_CONE_ID}/settings/security_level" \
+     -H "X-Auth-Email: ${CF_EMAIL_ADDRESS}" \
+     -H "X-Auth-Key: ${CF_API_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"value":"under_attack"}'
+}
+
+##
+# Check the current status
+##
+
+function ddos_check(){
+    if [ $ddos -gt $maxCPUload ]
+    then
+        if [ $currentStatus == "medium" ]
+        then
+        # Enable the CloudFlare DDOS protection
+        under_attack
+
+        echo "$(date) - Enabled DDoS" >> ~/.cloudflare/ddos.log 
+            if [ $notifications == 1 ] ; then
+                echo "$(date) - Enabled DDoS"  | mail -s "Enabled DDoS" ${CF_EMAIL_ADDRESS}
+            fi
+        else
+        exit 0
+        fi
+    elif [ $ddos -lt $normalCPUload ]
+    then
+        # If the CPU load is less than the normal CPU load for your server,
+        # then the DDoS protection would be disabled if the current status is under attack
+        if [ $currentStatus == "under_attack" ]
+        then
+            # Disable the CloudFlare DDOS protection
+            disable
+
+            echo "$(date) - Disabled DDoS" >> ~/.cloudflare/ddos.log 
+            if [ $notifications == 1 ] ; then
+                echo "$(date) - Disabled DDoS"  | mail -s "Enabled DDoS" bobby@bobbyiliev.com
+            fi
+        else
+        exit 0
+        fi
+    else
+    #echo "Everything is under control"
+    exit 0
+    fi
+}
+
+##
+# Call all functions
+##
+
+function main(){
+    allowed_cpu_load
+    status
+    ddos_check
+    rm -f ${status} ${current_status}
+}
+main
